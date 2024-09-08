b519aab7111412350013da1843256e5b9e0daffd
Hasna <hassna.ahmad98@gmail.com>
2024-09-08 12:09:41 +0300

    javascript lab

---
+++ main.js
@@ -0,0 +1,64 @@
+
+// Rectangle  Area 
+
+let width =  10, height = 5 ;
+let rectangleArea = width * height ; 
+console.log("Recangle Area = " + (rectangleArea))
+
+
+// tempreture converter 
+
+let currentTemp = 43 ; 
+let intoFeh = (currentTemp * 9/5) + 32;
+console.log(`The temperature is ${intoFeh} °F`);
+
+
+let fahrenheit = 94
+let celsius = (fahrenheit - 32) * 5/9;
+console.log(`The temperature is ${celsius} °C`);
+
+
+// sec counter 
+let hour = 1.5
+let sec = 1.5 * 3600;
+console.log(`The ${hour} has ${sec} seconds`)
+
+// Concatenation
+
+let thingOne = "Thing one" ;
+let thingtwo =  "Thing two"; 
+
+console.log(`Here are the ${thingOne} and the ${thingtwo}`); 
+
+// Total bill
+
+var bill = 10.25 + 3.99 + 7.15;
+var tip = bill * (15 / 100) ;
+var total = bill + tip ;
+console.log(`Total is ${total}`);
+
+// Mad Libs
+
+var adjective1 = "amazing";
+var adjective2 = "fun";
+var adjective3 = "entertaining";
+var midLab = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2}. I cannot wait to work through the rest of this ${adjective3} content!`
+
+console.log(midLab)
+
+// Awesome Message
+
+let firstName = "Hasna";
+let intrest = "watching movies";
+let hoppy = "exoplre new things "
+
+let awosemMessage = `Hi, my name is ${firstName}. I love ${intrest}. In my spare time, I like to ${hoppy}.`;
+
+console.log(awosemMessage);
+
+
+
+
+
+
+
